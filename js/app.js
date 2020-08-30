0d4a968bdaeac36e9129145e53417a801993343f
Vanesa GH <vanesagarridoh@gmail.com>
2020-08-30 00:41:15 -0500

    vue cli

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// Define a new component called button-counter
+Vue.component('button-counter', {
+    data: function() {
+        return {
+            count: 0
+        }
+    },
+    template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+})
+
+new Vue({ el: '#components-demo' })
